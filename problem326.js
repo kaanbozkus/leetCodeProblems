b99e6eceedf49ae0512f9de30de828bc74211b24
Kaan Bozkuş <98909523+kaanbozkus@users.noreply.github.com>
2022-06-09 16:38:09 +0300

    Problem 326 is solved.

---
+++ problem326.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+326. Power of Three
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+Example 1:
+Input: n = 27
+Output: true
+
+Example 2:
+Input: n = 0
+Output: false
+
+Example 3:
+Input: n = 9
+Output: true
+ 
+Constraints:
+-231 <= n <= 231 - 1
+
+*/
+
+const isPowerOfThree = function (n) {
+  if (n === 0) return false;
+
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+
+  return n === 1;
+};
+
+console.log(isPowerOfThree(82));
